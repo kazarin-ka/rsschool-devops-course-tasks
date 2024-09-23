4a0334b15f3b8154a6479d9f878ed9af7830b07c
Kirill Kazarin <ka.kazarin@gmail.com>
2024-09-24 00:05:16 +0200

    init commit: added basic configuration for terraform state storing

---
+++ 1_basic-configuration/init/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.68"
+    }
+  }
+  required_version = ">= 1.9.0"
+}
+
+# default
+provider "aws" {
+    alias   = "default"
+    region  = var.s3_bucket_region
+
+    default_tags {
+      tags = local.infra_tags
+    }
+}
+
+# alternative for specific resources like CloudFront
+provider "aws" {
+    alias   = "Virginia"
+    region  = "us-east-1"
+
+    default_tags {
+      tags = local.infra_tags
+    }
+}
---
+++ 1_basic-configuration/init/vars.tfvars
@@ -0,0 +1,7 @@
+env_type          = "dev"
+env_owner         = "kirill.kazarin"
+s3_bucket_region  = "eu-west-1" # Ireland
+s3_bucket_name    = "ka-kazarin-edu-eu-west-1-tfstates"
+dynamodb_tables   = [
+                      "ka-kazarin-edu-terraform-init",
+                    ]
