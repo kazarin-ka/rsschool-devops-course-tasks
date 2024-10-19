a72092b60a3b4f89a4f4aa1b9654773e10c1fe7c
Kirill Kazarin <ka.kazarin@gmail.com>
2024-10-19 17:35:43 +0200

    task_3: local test space/stand for checking k8s settings

---
+++ aux/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]],["vagrant-vboxmanage",["= 0.0.2"]]],"checksum":"016ca4c6ff2ea0d81176c5dfaf6376c5421c1f6de2bb784462375abb71f27486","vagrant_version":"2.4.1"}
